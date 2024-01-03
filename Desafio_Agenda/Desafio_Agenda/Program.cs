ee7af2a59fa35b03a8ac76522d7086ac2335d764
Lázaro Brito Fontenele <lazaro.fontenele.pro@prosoft.com.br>
2024-01-03 20:13:26 -0300

    Desafio 1 - implementação do menu

---
+++ Desafio_Agenda/Desafio_Agenda/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Agenda
+{
+    internal class Program
+
+    {
+        static string opcao = "";
+        static void mostrarMenu()
+        {
+            
+            while (opcao != "99")
+            {
+                Console.WriteLine("+----------------------------------------------------+\r\n" +
+                              "| Menu Principal                                     |\r\n" +
+                              "| Informe a opção desejada ou 99 para sair           |\r\n" +
+                              "|----------------------------------------------------|\r\n" +
+                              "| 1 = Cadastrar novo Contato na Agenda               |\n" +
+                              "| 2 = Excluir Contato da Agenda                      |\n" +
+                              "| 3 = Listar contatos da Agenda                      |\n" +
+                              "| 4 = Limpar todos os Contatos                       |\n" +
+                              "+----------------------------------------------------+");
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("AGENDA PESSOAL - MODULO 1");
+            mostrarMenu();
+            opcao = Console.ReadLine();
+            switch (opcao)
+            {
+                default:
+                    break;
+                case "99":
+                    Console.WriteLine("Finalizando programa...");
+                    break;
+                case "1":
+                    Console.WriteLine("VocÊ escolheu a opção 1");
+                    break;
+            }
+            Console.WriteLine("Programa Finalizado. Pressione qualquer tecla para fechar!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Estrutura_Sequencial/Estrutura_Sequencial/Exercícios.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_Sequencial
+{
+    internal class Exercícios
+    {
+        public static void Exercicio1()
+        {
+            int a = 2;
+            int b = 3;
+            int c = 5;
+
+            int x = a + b / c;
+            Console.WriteLine(x);
+            
+
+
+        }
+        public static void Exercicio2()
+        {
+            float nota1 = 6;
+            float nota2 = 8;
+            float nota3 =9;
+
+            float media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 10;
+            Console.WriteLine(Math.Round(media, 2));
+
+        }
+        public static void Exercicio3()
+        {
+            int segundosTotal = 4380;
+            int horaSegundos = 3600;
+
+            int horasEvento = segundosTotal / horaSegundos;
+            int minutosEvento = (segundosTotal % horaSegundos) / 60;
+            int segundosEvento = (segundosTotal % horaSegundos) % 60;
+
+            Console.WriteLine($"O evento ocorreu em: {horasEvento} hora(s) {minutosEvento} minuto(s) e {segundosEvento} segundo(s)");
+
+        }
+    }
+}
