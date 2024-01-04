cbd3d3db2615589e77ea57b65632e985e76ca49b
Lázaro Brito Fontenele <lazaro.fontenele.pro@prosoft.com.br>
2024-01-04 10:23:36 -0300

    Desafio Agenda - Metodos Incluir, Editar, Excluir

--- Desafio_Agenda/Desafio_Agenda/Program.cs
+++ Desafio_Agenda/Desafio_Agenda/Program.cs
@@ -10,10 +10,9 @@ namespace Desafio_Agenda
     internal class Program
 
     {
-        static string opcao = "";
-        static void mostrarMenu()
+        static void MostrarMenu()
         {
-            
+            string opcao = "";
             while (opcao != "99")
             {
                 Console.WriteLine("+----------------------------------------------------+\r\n" +
@@ -25,27 +24,104 @@ namespace Desafio_Agenda
                               "| 3 = Listar contatos da Agenda                      |\n" +
                               "| 4 = Limpar todos os Contatos                       |\n" +
                               "+----------------------------------------------------+");
+                opcao = Console.ReadLine();          
+                switch (opcao)
+                {
+                    default:
+                        break;
+                    case "99":
+                        Console.WriteLine("Finalizando programa...");
+                        break;
+                    case "1":
+                        GravarContato();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        ListarContatos();
+                        break;
+                    case "4":
+                       ExcluirContato();
+                       break;
+                }
             }
         }
-        static void Main(string[] args)
+
+        public static List<string> nomes = new List<string>();
+        public static List<int> telefones = new List<int>();
+        static void GravarContato()
+        {
+
+            if (nomes.Count >= 20)
+            {
+                
+                Console.WriteLine("Você atingiu a quantidade máxima de contatos. Não é permitido cadastrar mais de 20 contatos.");
+            }
+            else
+            {
+                Console.WriteLine("Digite o nome: ");
+                string nome = Console.ReadLine();
+                while (nome == "")
+                {
+                    Console.WriteLine("O nome não pode ser vazio");
+                    Console.WriteLine("Digite o nome: ");
+                    nome = Console.ReadLine();
+                }
+                Console.WriteLine("Digite o telefone: ");
+                int telefone = Convert.ToInt32(Console.ReadLine());
+                while (telefone is int == false)
+                {
+                    Console.WriteLine("Numero de telefone inválido. Tente novamente");
+                    Console.WriteLine("Digite o telefone: ");
+                    telefone = Convert.ToInt32(Console.ReadLine());
+                }
+                nomes.Add(nome);
+                telefones.Add(telefone);
+            }
+        }
+        static void ListarContatos()
+        {
+            if (nomes.Count > 0)
+            {
+                int i = 0;
+                foreach (var nome in nomes)
+                {
+                    Console.WriteLine($"Contato {i+1}: ");
+                    Console.WriteLine($"Nome: {nome}");
+                    Console.WriteLine($"Telefone: {telefones[i]}");
+                    Console.WriteLine("----------------------------");
+                    i++;
+                }
+            }
+        }
+        static void ExcluirContato()
+        {
+            Console.WriteLine("Digite o nome do contato que deseja excluir:");
+            string nome = Console.ReadLine();
+            if (nomes.Contains(nome))
+            {
+                int indice = nomes.IndexOf(nome);
+                telefones.RemoveAt(indice);
+                nomes.Remove(nome);
+                int removido = nomes.IndexOf(nome);
+                if (removido == -1) 
+                {
+                    Console.WriteLine("Contato excluído com sucesso");
+                }
+            }
+            else
+            {
+               Console.WriteLine("Contato não encontrado.");
+            }
+        }
+        public static void Main(string[] args)
         {
             
             Console.WriteLine("AGENDA PESSOAL - MODULO 1");
-            mostrarMenu();
-            opcao = Console.ReadLine();
-            switch (opcao)
-            {
-                default:
-                    break;
-                case "99":
-                    Console.WriteLine("Finalizando programa...");
-                    break;
-                case "1":
-                    Console.WriteLine("VocÊ escolheu a opção 1");
-                    break;
-            }
+            MostrarMenu();
             Console.WriteLine("Programa Finalizado. Pressione qualquer tecla para fechar!");
             Console.ReadKey();
+
         }
     }
 }
